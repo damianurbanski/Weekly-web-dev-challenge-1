cece39c5745b84e6224d34b9a4de19beefc2a3c5
Damian <damianurbanski97@gmail.com>
2018-08-06 22:56:33 +0200

    Final
    - index.html
    - icons
    - images
    - styles
    - gulp reconfig

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,25 @@
 var gulp = require('gulp'),
 cleanCSS = require("gulp-clean-css"),
 uglify = require("gulp-uglify"),
-concat = require("gulp-concat");
+concat = require("gulp-concat"),
+rename = require("gulp-rename");
 
 gulp.task('css', () => {
 	return gulp.src(['./css/**/*.css', '!./css/**/*.min.css'])
 	.pipe(cleanCSS({compatibility: 'ie8'}))
-	.pipe(concat('global.min.css'))
+     .pipe(rename({
+     	suffix: '.min'
+     }))
 	.pipe(gulp.dest('./css'))
 });
 
 gulp.task('js', () => {
 	return gulp.src(['js/**/*.js', '!js/**/*.min.js'])
 	.pipe(uglify())
-	.pipe(concat('main.min.js'))
 	.pipe(gulp.dest('./js'))
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', () => {
   gulp.watch(['./css/**/*.css', '!./css/**/*.min.css'], ['css']);
   gulp.watch(['js/**/*.js', '!js/**/*.min.js'], ['js']);
 });
